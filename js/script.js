40c0e26230719b88c4fce3ada7e75abf4a39f34a
Luis <luisrosero098@gmail.com>
2018-02-06 19:59:06 +0000

    final updates_

--- js/script.js
+++ js/script.js
@@ -7,19 +7,7 @@
 // Document Ready Function. All of your jQuery should go in here. 
 /* global $ */ 
 
-
 $( document ).ready(function() {
-var movementStrength = 25;
-var height = movementStrength / $(window).height();
-var width = movementStrength / $(window).width();
-$("#background_Container").mousemove(function(e){
-          var pageX = e.pageX - ($(window).width() / 2);
-          var pageY = e.pageY - ($(window).height() / 2);
-          var newvalueX = width * pageX * -1 - 25;
-          var newvalueY = height * pageY * -1 - 50;
-          $('#background_Container').css("background-position", newvalueX+"px"+newvalueY+"px");
-
-/* Container Latinizer_Blw*/ 
 
 	$('#btn').click(function(){ 
 		// $('#inp').append("ay")
